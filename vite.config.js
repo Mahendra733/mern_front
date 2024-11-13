9e2d7026d8d56fdfec22bd1865048ebf49d2bd73
Mahendra733 <mahendrachowdhary733@gmail.com>
2024-11-13 12:34:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:3000",
-      "/uploads/": "http://localhost:3000",
+      "/api/": "https://mern-back-63eo.onrender.com/",
+      "/uploads/": "https://mern-back-63eo.onrender.com/",
     },
   },
 });
